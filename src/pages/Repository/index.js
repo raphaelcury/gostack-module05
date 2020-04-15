c1e1584542adc5427017dae85692372e965a25c2
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-04-15 19:50:32 -0300

    Repository component transformed into class.

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // import { Container } from './styles';
 
-export default function Repository({ match }) {
-  return <h1>Repository {decodeURIComponent(match.params.repository)}</h1>;
+export default class Repository extends Component {
+  constructor() {
+    super();
+    this.setState({
+      repository: {},
+      issues: [],
+    });
+  }
+
+  render() {
+    const { match } = this.props;
+    return <h1>Repository {decodeURIComponent(match.params.repository)}</h1>;
+  }
 }
